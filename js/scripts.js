064570167c16f918fa4ed70104483cd62598721f
dmortkowitz <dmortkowitz@gmail.com>
2018-08-17 11:49:47 -0700

    Add first check for zero, but results not displaying for any input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
-(document).ready(function(){
-    $("form#userNumber").submit(function(event){
-    event.preventDefault();
 
+userZero(input)
+  var result = input.toString()indexOf(0);
+  if(result === -1){
+    return false;
+  }else{
+    return true;
+  }
+
+changeInput(input){
+  var numberArr = [];
+  for (var i=0; i <= input; i++){
+    if (userZeroes[i] === true) {
+      numberArr.push("Beep!");
+    }
+  }
+  return numberArr;
+}
+
+
+(document).ready(function(){
+  $("form#userNumber").submit(function(event){
+  event.preventDefault();
+  var userInput = $("#userInput").val();
+  var resultArr = changeInput(inputNumber);
+  var result = " ";
+  for (var i=0; i<resultArr.length;i++){
+    result += resultArr[i] + " ";
+  }
+    // $("#output").text(result);
+    alert(result);
   });
 });
